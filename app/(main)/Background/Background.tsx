cc658d54d9e9218cbdc86488068a552aaa209244
corey <corey.cushnie@gmail.com>
2024-07-10 23:09:29 -0400

    bg

---
+++ app/(main)/Background/Background.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+type Props = {};
+
+export default function Background({}: Props) {
+  return (
+    <div className="background">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
