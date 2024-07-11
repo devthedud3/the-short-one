d355d3a8d43b7fc6b63fc24f33d508c2d416e1cc
corey <corey.cushnie@gmail.com>
2024-07-11 10:47:26 -0400

    build

--- app/api/[id]/route.ts
+++ app/api/[id]/route.ts
@@ -1,13 +1,18 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { pool } from "@/database/client";
 import { NextResponse } from "next/server";
 
-const urlDatabase: any = {};
+interface UrlDatabase {
+  id: string;
+}
 
-export async function GET(req: Request, res: { params: { id: string } }) {
+export async function GET(req: Request, context: UrlDatabase) {
   //@ts-ignore
-  const { id } = res.params;
+  const { id } = context.params;
 
-  console.log(res.params.id);
-
-  return NextResponse.json({ longUrl: "https://google.com" });
+  const response = await pool.query("SELECT * FROM urls WHERE id = $1", [id]);
+  const { rows } = await response;
+  if (rows.length !== 0) {
+    return NextResponse.json({ longUrl: rows[0].long_url });
+  }
+  return NextResponse.json({ longUrl: [] });
 }
