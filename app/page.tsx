d3caec84d2345dcacbe76bf75f87b2201ef043c4
corey <corey.cushnie@gmail.com>
2024-07-12 17:45:26 -0400

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,18 @@ import { Shortener } from "./(main)";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen p-36 w-full justify-center">
+    <main className="flex min-h-screen p-48 w-full justify-center">
       <div className="flex flex-col items-center space-y-10">
-        <p className="border px-5 py-2 rounded-full font-bold bg-lime-200 text-lime-700 ">
+        <p className="border px-5 py-2 text-xs rounded-full font-bold bg-lime-200 text-lime-700 ">
           Simplify your link while increasing your visibility
         </p>
-        <div className="relative flex items-end">
-          <h2 className="text-6xl font-bold text-transparent font-outline-1 mb-24">
+        <div className="flex flex-col pb-24 text-center">
+          <h2 className="text-7xl font-bold text-transparent font-outline-1">
             THE SHORTER ONE
           </h2>
+          <p className="text-xs">
+            Enter a URL, make it shorter, and see what we mean.
+          </p>
         </div>
         <Shortener />
       </div>
