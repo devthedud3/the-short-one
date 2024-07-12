c83668e325b1dfd0c76dccc64a09121434d8bb78
corey <corey.cushnie@gmail.com>
2024-07-12 16:36:17 -0400

    utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,3 +5,16 @@ export function getPath() {
   }
   return "";
 }
+
+export function isValidLink(e: string) {
+  const newURL = e.replace(/^(www\.|https?:\/\/(www\.)?)/, "");
+  try {
+    const re = /^(www\.|https?:\/\/)\S+(\.\w{2,}|\/\S*)$/;
+    const { href, origin } = new URL("http://" + newURL);
+    if (re.test(`${origin}/`)) return href;
+    return "";
+  } catch (e) {
+    console.log("Check isValidLink function", e);
+    return "";
+  }
+}
