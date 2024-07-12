2214b6af5e5080619fc1ee71b1e2738646877964
corey <corey.cushnie@gmail.com>
2024-07-12 16:36:08 -0400

    api

--- app/api/[id]/route.ts
+++ app/api/[id]/route.ts
@@ -11,8 +11,29 @@ export async function GET(req: Request, context: UrlDatabase) {
 
   const response = await pool.query("SELECT * FROM urls WHERE id = $1", [id]);
   const { rows } = await response;
-  if (rows.length !== 0) {
-    return NextResponse.json({ longUrl: rows[0].long_url });
+
+  if (rows.length > 0) {
+    return NextResponse.json({
+      longUrl: rows[0].long_url,
+      visits: rows[0].visits,
+    });
   }
   return NextResponse.json({ longUrl: [] });
+}
+
+export async function PUT(req: Request, context: UrlDatabase) {
+  //@ts-ignore
+  const { id } = context.params;
+
+  const query = `
+  UPDATE urls
+  SET visits = visits + 1
+  WHERE id = $1;
+`;
+  const response = await pool.query(query, [id]);
+  const { rows } = await response;
+  // if (rows.length !== 0) {
+  //   return NextResponse.json({ longUrl: rows[0].long_url });
+  // }
+  return NextResponse.json({ message: "A new visitor!" });
 }
