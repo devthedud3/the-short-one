c6c8983049f2d0f45360fda4db8d620f0998a394
corey <corey.cushnie@gmail.com>
2024-07-12 16:36:12 -0400

    api

--- app/api/create/route.ts
+++ app/api/create/route.ts
@@ -2,21 +2,20 @@ import { NextResponse } from "next/server";
 import { nanoid } from "nanoid";
 import { pool } from "@/database/client";
 
-const urlDatabase: any = {};
-
 export async function POST(req: Request, res: Response) {
   const { origin } = new URL(req.url);
-  try {
-    const { longUrl } = await req.json();
-    const id = nanoid(7);
 
+  try {
+    const { newUrl } = await req.json();
+
+    const id = nanoid(5);
     const { rows } = await pool.query(
       "SELECT * FROM urls WHERE long_url = $1",
-      [longUrl]
+      [newUrl]
     );
 
     if (rows.length === 0) {
-      await pool.query("INSERT INTO urls VALUES ($1, $2, 0)", [id, longUrl]);
+      await pool.query("INSERT INTO urls VALUES ($1, $2, 0)", [id, newUrl]);
 
       return NextResponse.json({
         shortUrl: `${origin}/${id}`,
