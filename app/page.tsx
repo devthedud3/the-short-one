b63973b6825b9085e231e7ddcf46b142bcf915b4
corey <corey.cushnie@gmail.com>
2024-07-12 17:23:29 -0400

    page build

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,19 @@ import { Shortener } from "./(main)";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen p-24 ">
-      <div className="flex flex-col ">
-        <h1 className="text-5xl font-bold py-32 gradient-text">
-          THE SHORTER ONE
-        </h1>
+    <main className="flex min-h-screen p-36 w-full justify-center">
+      <div className="flex flex-col items-center space-y-10">
+        <p className="border px-5 py-2 rounded-full font-bold bg-lime-200 text-lime-700 ">
+          Simplify your link while increasing your visibility
+        </p>
+        <div className="relative flex items-end">
+          <h2 className="text-6xl font-bold text-transparent font-outline-1 mb-24">
+            THE SHORTER ONE
+          </h2>
+        </div>
         <Shortener />
       </div>
-      <div></div>
+      {/* <div></div> */}
     </main>
   );
 }
