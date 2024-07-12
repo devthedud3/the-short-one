30db579f72560a41cd95b918bacd436973440bed
corey <corey.cushnie@gmail.com>
2024-07-12 16:35:32 -0400

    page build

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,14 @@ import { Shortener } from "./(main)";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-5xl font-bold py-32 text-white">THE SHORTER ONE</h1>
-      <Shortener />
+    <main className="flex min-h-screen p-24 ">
+      <div className="flex flex-col ">
+        <h1 className="text-5xl font-bold py-32 gradient-text">
+          THE SHORTER ONE
+        </h1>
+        <Shortener />
+      </div>
+      <div></div>
     </main>
   );
 }
