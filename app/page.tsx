80be31c635b61470fd70333cdbe2e6f086942a52
corey <corey.cushnie@gmail.com>
2024-07-12 17:54:59 -0400

    padding

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Shortener } from "./(main)";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen p-48 w-full justify-center">
+    <main className="flex min-h-screen p-12 lg:m-0 lg:p-48 w-full justify-center">
       <div className="flex flex-col items-center space-y-10">
         <p className="border px-5 py-2 text-xs rounded-full font-bold bg-lime-200 text-lime-700 ">
           Simplify your link while increasing your visibility
