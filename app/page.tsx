296bfebc9c23de0344c17e2cf89f59391ce44d3a
corey <corey.cushnie@gmail.com>
2024-07-12 18:12:51 -0400

    add github link

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,25 @@
 import Image from "next/image";
 import { Shortener } from "./(main)";
+import { VscGithub } from "react-icons/vsc";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen p-12 lg:m-0 lg:p-48 w-full justify-center">
+      <Link
+        className="fixed top-4 right-4 cursor-pointer hover:animate-pulse"
+        href={`${process.env.PROJECT_LINK}`}
+        target="_blank"
+      >
+        <VscGithub size={30} />
+      </Link>
       <div className="flex flex-col items-center space-y-10">
         <p className="border px-5 py-2 text-xs rounded-full font-bold bg-lime-200 text-lime-700 ">
           Simplify your link while increasing your visibility
         </p>
         <div className="flex flex-col pb-24 text-center items-center space-y-2">
           <h2 className="text-7xl font-bold text-transparent font-outline-1">
-            THE SHORTER ONE
+            THE SHORT ONE
           </h2>
           <p className="text-xs">
             Enter a URL, make it shorter, and watch it grow
