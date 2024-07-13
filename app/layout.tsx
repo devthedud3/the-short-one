d7276d1b914ffd5439bfe1025c7b920cfc4ba6de
corey <corey.cushnie@gmail.com>
2024-07-13 10:00:40 -0400

    page build

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "URL Shortener",
+  title: "The Short One - URL Shortener",
   description: "Created by Corey",
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex px-12 py-20 lg:m-0 lg:p-48 w-full justify-center">
-      <div className="absolute px-12 flex justify-between max-w-[1170px] w-full top-4">
+      <div className="absolute px-4 flex justify-between w-full top-4">
         <p className="font-black text-2xl">
           SHORT
           <span className="text-lime-500 border-b-1 text-outline-1 text-tr border-black">
@@ -23,13 +23,15 @@ export default function Home() {
       </div>
       <div className="flex flex-col items-center space-y-10">
         <p className="border px-5 py-2 text-xs rounded-full font-bold bg-lime-300 text-lime-700 ">
-          Shrink your links, expand your reach!
+          The Ultimate URL Shortener
         </p>
         <div className="flex flex-col pb-24 text-center items-center space-y-2">
           <h2 className="text-7xl font-bold text-transparent font-outline-1">
             THE SHORT ONE
           </h2>
-          <p className="text-xs">Shrink your link, expand your reach!</p>
+          <p className="text-xs">
+            Shrink your link, share with friends, and expand your reach!
+          </p>
         </div>
         <Shortener />
       </div>
