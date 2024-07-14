ef3a2fa5081132e6ca7f8da55483b27949708f64
corey <corey.cushnie@gmail.com>
2024-07-14 16:37:22 -0400

    regex fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,12 +7,14 @@ export function getPath() {
 }
 
 export function isValidLink(url: string) {
-  const formattedURL = url.replace(/^(www\.|https?:\/\/(www\.)?)/, "");
+  const formattedURL = url.replace(/^(www\.|https?:\/\/(www\.)?)|(\/)/, "");
   try {
-    const re = /^(www\.|https?:\/\/)\S+(\.\w{2,}|\/\S*)$/;
-    const { href, origin } = new URL(`http://${formattedURL}/`);
+    const re = /^(www\.|https?:\/\/)\S+(\.\w{2,})/;
+    const u = new URL(`http://${formattedURL}`);
+    const { href, origin } = new URL(`http://${formattedURL}`);
 
-    if (re.test(`${origin}/`)) return href;
+    console.log(formattedURL, u);
+    if (re.test(`${origin}/`)) return href.slice(0, -1);
 
     return "";
   } catch (error: any) {
